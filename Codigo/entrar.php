b9477146083a5210d836cadaec190e3dae61b63e
Raffael Oliveira <80132535+RaffaelOliveiraPuc@users.noreply.github.com>
2021-05-25 21:18:24 -0300

    Add files via upload
    Versão do entrar com style dos erros e avisos de interação do usuário

--- Codigo/entrar.php
+++ Codigo/entrar.php
@@ -10,7 +10,8 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <script src="https://kit.fontawesome.com/c6addf5154.js" crossorigin="anonymous"></script>
 
@@ -28,8 +29,10 @@ session_start();
                     <div class="col-6 divSearch">
 
                         <form class="formSearch form-inline">
-                            <input class="inputSearch form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+                            <input class="inputSearch form-control mr-sm-2" type="search" placeholder="Search"
+                                aria-label="Search">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i
+                                    class="fas fa-search"></i></button>
                         </form>
 
                     </div>
@@ -39,14 +42,17 @@ session_start();
                 </div>
                 <div class="divNavbar row">
                     <nav class="col-12 navbar navbar-expand-lg navbar-light bg-light">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
 
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav mr-auto">
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         Categorias
                                     </a>
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -58,31 +64,31 @@ session_start();
                                 </li>
 
                                 <?php if (isset($_SESSION['email'])) : ?>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">Meus anúncios <span class="sr-only"></span></a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">Minhas propostas <span class="sr-only"></span></a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">Sobre <span class="sr-only"></span></a>
-                                    </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Meus anúncios <span class="sr-only"></span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Minhas propostas <span class="sr-only"></span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Sobre <span class="sr-only"></span></a>
+                                </li>
                                 <?php else : ?>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">Sobre <span class="sr-only"></span></a>
-                                    </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Sobre <span class="sr-only"></span></a>
+                                </li>
                                 <?php endif; ?>
 
                             </ul>
                             <form class="form-inline my-2 my-lg-0">
                                 <?php if (isset($_SESSION['email'])) : ?>
-                                    <p>
-                                        <?php echo ($_SESSION['email']); ?>
-                                    </p>
-                                    <a class="nav-link" href="logout.php">Sair<span class="sr-only"></span></a>
+                                <p>
+                                    <?php echo ($_SESSION['email']); ?>
+                                </p>
+                                <a class="nav-link" href="logout.php">Sair<span class="sr-only"></span></a>
                                 <?php else : ?>
-                                    <a class="nav-link" href="entrar.php">Entrar<span class="sr-only"></span></a>
-                                    <a class="nav-link" href="cadastrar.php">Cadastrar<span class="sr-only"></span></a>
+                                <a class="nav-link" href="entrar.php">Entrar<span class="sr-only"></span></a>
+                                <a class="nav-link" href="cadastrar.php">Cadastrar<span class="sr-only"></span></a>
                                 <?php endif; ?>
                             </form>
                         </div>
@@ -124,9 +130,13 @@ session_start();
                     <?php
                     if (isset($_SESSION['nao_autenticado'])) :
                     ?>
-                        <div>
-                            <h5>Email ou senha inválidos.</h5>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 divAvisoErro">
+                                <h5>Email ou senha inválidos.</h5>
+                            </div>
                         </div>
+                    </div>
                     <?php
                     endif;
                     unset($_SESSION['nao_autenticado']);
@@ -135,9 +145,13 @@ session_start();
                     <?php
                     if (isset($_SESSION['vazio'])) :
                     ?>
-                        <div>
-                            <h5>Necessário preencher todos os campos.</h5>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 divAvisoErro">
+                                <h5>Necessário preencher todos os campos.</h5>
+                            </div>
                         </div>
+                    </div>
                     <?php
                     endif;
                     unset($_SESSION['vazio']);
@@ -156,9 +170,15 @@ session_start();
         </div>
     </footer>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
 
 </body>
 
