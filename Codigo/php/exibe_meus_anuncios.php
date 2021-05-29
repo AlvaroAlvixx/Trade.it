11a8587e4f37f3b225555cc11956f753b27273a0
Álvaro Augusto <65177369+AlvaroAlvixx@users.noreply.github.com>
2021-05-29 02:13:56 -0300

    Add files via upload
    Função exibir anúncios do usuario

---
+++ Codigo/php/exibe_meus_anuncios.php
@@ -0,0 +1,21 @@
+<?php
+include("conexao.php");
+session_start();
+
+
+$email = $_SESSION['email'];
+$query = "select cpf from usuarios where email = '$email'";
+$result = mysqli_query($conexao, $query);
+$cpf = mysqli_fetch_assoc($result);
+$valor = $cpf['cpf'];
+
+$query = "select titulo, descricao, status, imagens from anuncios where usuario_cpf = '$valor'";
+$result = mysqli_query($conexao, $query);
+
+if (($result) and ($result->num_rows != 0)) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo $row['titulo'] . "|" . $row['descricao'] . "|" . $row['status'] . "|" . $row['imagens'] . "|";
+    }
+} else {
+    echo "";
+}
