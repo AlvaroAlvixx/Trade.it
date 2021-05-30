4991495f9c79a0fed7cdef2aea545b2e6bd61de7
Álvaro Augusto <65177369+AlvaroAlvixx@users.noreply.github.com>
2021-05-30 16:06:19 -0300

    Add files via upload

---
+++ Codigo/js/minhas_propostas.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    $.post('php/exibe_minhas_propostas.php', function (retorna) {
+        let retornou = retorna;
+        console.log(retornou);
+
+        if (retornou == "") {
+            $("#minhasPropostas").html("Nenhum anúncio encontrado");
+        } else {
+
+            let valor = []
+            valor = (retornou.split("|"));
+
+            let texto = '';
+            let i = 0;
+
+            while (i < (valor.length - 1)) {
+                texto = texto + `<fieldset class="fieldsetProposta">
+                <div class="row">
+                    <div class="col-2 divImagem">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="imagem">
+                                <img src="${valor[(i + 2)]}" class="card-img-top">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-8">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="infoProposta">
+                                    <div class="row">
+                                        <label for="anuncio" class="lblInfo"> Anúncio: </label>
+                                        <label id="anuncio">${valor[i]}</label >
+                                    </div >
+                                    <div class="row">
+                                        <label for="proposta" class="lblInfo"> Proposta: </label>
+                                        <label id="proposta">${valor[i + 1]}</label>
+                                    </div>
+                                    <div class="row">
+                                        <label for="status" class="lblInfo"> Status: </label>
+                                        <label id="status"></label>
+                                    </div>
+                                </div >
+                            </div >
+                        </div >
+                    </div >
+                    <div class="col-2">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="divBtnVerificar">
+                                <a href="proposta.php" onclick="define_proposta('${valor[(i + 2)]}')">
+                                    <button type="submit" id="btnVerificarProposta" class="btn btn-primary btnVerificarProposta">Verificar</button>
+                                </a>
+                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                                </div >
+                            </fieldset >
+                    `
+                $("#minhasPropostas").html(texto);
+                i = i + 3;
+            }
+        }
+
+    });
+});
+
