6d84d070e04cbe4641843ff77dc2eeeeff378ef3
Álvaro Augusto <65177369+AlvaroAlvixx@users.noreply.github.com>
2021-06-07 21:12:55 -0300

    Add files via upload

--- Codigo/js/anuncio.js
+++ Codigo/js/anuncio.js
@@ -30,6 +30,7 @@ $(document).ready(function () {
 
     $.post('php/anuncio_dados.php', function (retorna) {
         let retornou = retorna;
+
         let valor = [];
         valor = (retornou.split("|"));
 
@@ -39,6 +40,9 @@ $(document).ready(function () {
         let imagens = (valor[3]);
         let status = (valor[4]);
         let condicao = (valor[5]);
+        let nomeOfertante = (valor[6]);
+        let emailOfertante = (valor[7]);
+        let logado = (valor[8]);
         let statusNome = "";
 
         if (condicao == "1")
@@ -59,6 +63,11 @@ $(document).ready(function () {
         $("#id_condicao").html(condicoes);
         $("#id_descricao").html(descricao);
         $("#id_imagem").html(`<img src="${imagens}" class="card-img-top">`);
+        $("#id_ofertante").html(nomeOfertante);
+
+        if (logado != "" && logado != emailOfertante) {
+            $("#btn_troca").html(`<button id="btnTrocar" type="submit" class="btn btn-primary btnNovoAnuncio">Propor troca</button>`);
+        }
     });
 });
 
